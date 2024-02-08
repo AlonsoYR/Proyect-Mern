033d28666155e12d9b00b0202bb55275da8ac8dd
AlonsoYR <alonso001029@gmail.com>
2024-02-08 12:48:00 -0500

    Video 5
    Rutas

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
-app.get('/users', (req, res) => res.send('Users Routes'))
-app.get('/notes', (req, res) => res.send('Notes Routes'))
+app.use('/users',require('./routes/users'))
+app.use('/notes',require('./routes/notes'))
 
 module.exports = app;
 

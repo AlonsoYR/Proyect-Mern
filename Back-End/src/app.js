ec2c78482dba96b41ca629cba718909130a2d841
AlonsoYR <alonso001029@gmail.com>
2024-02-08 12:31:39 -0500

    Video 4

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 //Settings 
+app.set('port', process.env.PORT || 4000);
 
 //Middlewares
+app.use(cors());
+app.use(express.json());
 
 //Routes
+app.get('/users', (req, res) => res.send('Users Routes'))
+app.get('/notes', (req, res) => res.send('Notes Routes'))
 
 module.exports = app;
 

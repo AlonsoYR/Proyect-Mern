6950d22cc9d341f0f7eedba53c086f8f6a248df6
AlonsoYR <alonso001029@gmail.com>
2024-02-08 21:18:49 -0500

    Video 10

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class CreateUser extends Component {
+
+    state = {
+        users: []
+    } 
+    
+    async componentDidMount(){
+        const res = await axios.get('http://localhost:4000/users');
+        this.setState({users: res.data});
+        console.log(this.setState.users)
+    }
+
+
   render() {
     return (
-      <div>
-        User Create
+      <div className='row'>
+        <div className="col-md-4">
+            Form User
+        </div>
+        <div className="col-md-8">
+            <ul className="list-group">
+                {
+                    this.state.users.map(users => <li className='list-group-item list-group-item-action' key={users._id}>
+                        {users.usermane}
+                    </li>)
+                }
+            </ul>
+        </div>
       </div>
     )
   }
